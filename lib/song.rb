d0809d4b525ee41387e2a8e6bd89ab0d70d95334
Caio Sanches <ccampato@gmail.com>
2018-06-04 19:21:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,8 +16,9 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song.name = filename.split(" - ")[0]
+    song_name = filename.split(" - ")[0]
     song = Song.new(song_name)
+    song.name= song_name
     song
 
   end
