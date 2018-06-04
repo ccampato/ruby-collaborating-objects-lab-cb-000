e3c6f6357949c3da754dcd16caa3c44d1fabc4ce
Caio Sanches <ccampato@gmail.com>
2018-06-04 18:44:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,8 @@ class Song
 
   def self.new_by_filename(file)
     file.each do |entry|
-      song_name= entry.split(" - ")[1]
-      artist_name= entry.split(" - ")[0]
+      song_name= entry.split(" - ")[0]
+      artist_name= entry.split(" - ")[1]
 
       new_instance = Song.new(song_name)
       new_instance.artist_name= artist_name
