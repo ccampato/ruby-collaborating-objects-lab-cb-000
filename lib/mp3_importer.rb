9adcd0d40cf3d900cf664e36dba4c1ce73611cb9
Caio Sanches <ccampato@gmail.com>
2018-06-04 19:01:34 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,8 +9,7 @@ class MP3Importer
     Dir.entries(@path).select { |f| !File.directory? f }
   end
 
-  def import
-    file =  self.files
-    Song.new_by_filename(file)
+  def import(files)
+    files.each { |file| Song.new_by_filename(file)}
   end
 end
