b74846849c01811c9ff0abee54cd6c4bc2620b02
Caio Sanches <ccampato@gmail.com>
2018-06-04 18:40:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     artist= Artist.find_or_create_by_name(name)
   end
 
-  def self.new_by_filename(filename)
+  def self.new_by_filename(file)
     file.each do |entry|
       song_name= entry.split(" - ")[1]
       artist_name= entry.split(" - ")[0]
